82891b3e0a4285dcb949d3dacdd4231ab4e48283
jtr354 <jtr354@foxmail.com>
2024-01-14 11:53:41 +0800

    feat: mini-react: transform vnode to fiber

--- packages/mini-react/core/react-dom.js
+++ packages/mini-react/core/react-dom.js
@@ -1,34 +1,10 @@
-import { TEXT_ELEMENT } from "./constants.js";
-
-function render(vnode, el) {
-  // function component
-  if (typeof vnode.type === "function") {
-    render(vnode.type(vnode.props), el);
-    return;
-  }
-  const dom =
-    vnode.type === TEXT_ELEMENT
-      ? document.createTextNode("")
-      : document.createElement(vnode.type);
-
-  Object.keys(vnode.props).forEach((key) => {
-    if (key !== "children") {
-      dom[key] = vnode.props[key];
-    }
-  });
-
-  vnode.props.children.forEach((child) => {
-    render(child, dom);
-  });
-
-  el.append(dom);
-}
+import React from "./react";
 
 export default {
   createRoot(domNode, options) {
     return {
       render(reactNode) {
-        render(reactNode, domNode);
+        React.render(reactNode, domNode);
       },
     };
   },
--- packages/mini-react/core/react.js
+++ packages/mini-react/core/react.js
@@ -22,6 +22,89 @@ export function createTextNode(nodeValue) {
   };
 }
 
+export function render(vnode, el) {
+  console.log(vnode);
+  nextUnitOfWork = {
+    dom: el,
+    props: {
+      children: [vnode],
+    },
+  };
+}
+
+function createFiber(vnode, parent = null) {
+  if (typeof vnode.type === "function") {
+    vnode = vnode.type(vnode.props);
+  }
+  return {
+    child: null,
+    sibling: null,
+    parent,
+    type: vnode.type,
+    props: vnode.props,
+  };
+}
+
+let nextUnitOfWork = null;
+function workLoop(idleDeadline) {
+  let shouldYield = false;
+  while (!shouldYield && nextUnitOfWork) {
+    nextUnitOfWork = performanceUnitWork(nextUnitOfWork);
+
+    shouldYield = idleDeadline.timeRemaining() < 1;
+  }
+  requestIdleCallback(workLoop);
+}
+requestIdleCallback(workLoop);
+
+function performanceUnitWork(work) {
+  console.log(work);
+  // 1. create dom
+  if (!work.dom) {
+    const dom = (work.dom = createDOM(work.type));
+    // 2. update props
+    updateProps(work.props, dom);
+    work.parent.dom.append(dom);
+  }
+  // 3. transform vnode to fiber
+  initialChildren(work);
+  // 4. return next work
+  if (work.child) {
+    return work.child;
+  }
+  if (work.sibling) {
+    return work.sibling;
+  }
+  return work.parent?.sibling;
+}
+
 export default {
   createElement,
+  render,
 };
+function updateProps(props, dom) {
+  Object.keys(props).forEach((key) => {
+    if (key !== "children") {
+      dom[key] = props[key];
+    }
+  });
+}
+
+function createDOM(type) {
+  return type === TEXT_ELEMENT
+    ? document.createTextNode("")
+    : document.createElement(type);
+}
+
+function initialChildren(work) {
+  let prevChild = null;
+  work.props.children.forEach((child, index) => {
+    const fiber = createFiber(child, work);
+    if (index === 0) {
+      work.child = fiber;
+    } else {
+      prevChild.sibling = fiber;
+    }
+    prevChild = fiber;
+  });
+}
