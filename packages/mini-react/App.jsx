4b297dc2695b339c3fbb22335fec7618d8f3dfd4
jtr354 <jtr354@foxmail.com>
2024-01-16 23:03:42 +0800

    feat: mini-react: commit

--- packages/mini-react/App.jsx
+++ packages/mini-react/App.jsx
@@ -1,11 +1,26 @@
 /**@jsx CReact.createElement */
 import CReact from "./core/react.js";
 
+const Child = () => {
+  return <div>child</div>;
+};
+
+const Counter = ({ num }) => {
+  return (
+    <div>
+      {num}
+      <Child></Child>
+    </div>
+  );
+};
+
 const App = () => (
   <div className="red">
     <h1>mini-react</h1>
     <input placeholder="mini-react" />
     <button>add</button>
+    <Counter num="1" />
+    <Counter num="12" />
   </div>
 );
 
--- packages/mini-react/core/react.js
+++ packages/mini-react/core/react.js
@@ -22,14 +22,15 @@ export function createTextNode(nodeValue) {
   };
 }
 
+let root = null;
 export function render(vnode, el) {
-  console.log(vnode);
   nextUnitOfWork = {
     dom: el,
     props: {
       children: [vnode],
     },
   };
+  root = nextUnitOfWork;
 }
 
 function createFiber(vnode, parent = null) {
@@ -50,21 +51,36 @@ function workLoop(idleDeadline) {
   let shouldYield = false;
   while (!shouldYield && nextUnitOfWork) {
     nextUnitOfWork = performanceUnitWork(nextUnitOfWork);
+    if (!nextUnitOfWork && root) {
+      commitRoot();
+    }
 
     shouldYield = idleDeadline.timeRemaining() < 1;
   }
   requestIdleCallback(workLoop);
 }
+
+function commitRoot() {
+  commitWork(root.child);
+  root = null;
+}
+
+function commitWork(fiber) {
+  if (!fiber) return;
+  fiber.parent.dom.append(fiber.dom);
+  commitWork(fiber.child);
+  commitWork(fiber.sibling);
+}
+
 requestIdleCallback(workLoop);
 
 function performanceUnitWork(work) {
-  console.log(work);
   // 1. create dom
   if (!work.dom) {
     const dom = (work.dom = createDOM(work.type));
     // 2. update props
     updateProps(work.props, dom);
-    work.parent.dom.append(dom);
+    // work.parent.dom.append(dom);
   }
   // 3. transform vnode to fiber
   initialChildren(work);
